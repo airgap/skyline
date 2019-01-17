229cb00bb597deabc5765a0c2d3dab48e53fe3ea
Nick Muzzin <nick.muzzin@gmail.com>
2019-01-17 01:49:02 -0500

    Extremely short but now uses global variables

--- skyline.js
+++ skyline.js
@@ -1,20 +1,6 @@
-function skyline(array) {
-  var array2 = [], counter = 0, i, j;
-  for(i of array)
-    array2[i] = (array2[i] || 0) + 1;
-  for(i in array2)
-    for(j = 0; j < array2[i]; j++)
-      array[counter++]=i*1;
-  return array;
+function skyline(a) {
+  b=[],c=0
+  for(i of a)b[i]=!!b[i]+1
+  for(i in b)for(;b[i]--;)a[c++]=i*1
+  return a
 }
-
-var rndarray = [];
-
-for(var i = 0; i < 1000;i++)
-  rndarray[i]=~~(Math.random()*1000);
-
-console.log(rndarray)
-
-var sorted = skyline(rndarray);
-
-console.log(sorted)
