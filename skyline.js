700ed238b2862a00d1ed3922f597bcb0077c3960
Nick Muzzin <nick.muzzin@gmail.com>
2019-01-17 02:07:26 -0500

    Removed the test case

--- skyline.js
+++ skyline.js
@@ -4,15 +4,3 @@ function skyline(a) {
   for(i in b)for(;b[i]--;)a[c++]=i*1
   return a
 }
-
-
-var rndarray = [9,8,7,6,5,4,3,2,1];
-
-for(var i = 0; i < 10;i++)
-  rndarray[i]=~~(Math.random()*3);
-
-console.log(rndarray)
-
-var sorted = skyline(rndarray);
-
-console.log(sorted)
