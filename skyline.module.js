50e941aa330fbf60318645abbb7e9d5a63496c27
Nick Muzzin <nick.muzzin@gmail.com>
2019-01-17 00:25:49 -0500

    Updated to match skyline.js

--- skyline.module.js
+++ skyline.module.js
@@ -1,13 +1,11 @@
 exports = function(array) {
   var array2 = [];
-  for(var i in array) {
-    var j = array[i];
+  for(var j of array)
     array2[j] = (array2[j] || 0) + 1;
-  }
   array = [];
   var counter = 0;
   for(var i in array2)
     for(var j = 0; j < array2[i]; j++)
       array[counter++]=i*1;
     return array;
-};
+}
