9319a51fe4a37c815d4ec50ce20af138fb76a557
Nick Muzzin <nick.muzzin@gmail.com>
2019-01-17 01:33:12 -0500

    Yet more concise!

--- skyline.js
+++ skyline.js
@@ -1,9 +1,20 @@
 function skyline(array) {
-  var array2 = [], counter = 0;
-  for(var j of array)
-    array2[j] = (array2[j] || 0) + 1;
-  for(var i in array2)
-    for(var j = 0; j < array2[i]; j++)
+  var array2 = [], counter = 0, i, j;
+  for(i of array)
+    array2[i] = (array2[i] || 0) + 1;
+  for(i in array2)
+    for(j = 0; j < array2[i]; j++)
       array[counter++]=i*1;
   return array;
 }
+
+var rndarray = [];
+
+for(var i = 0; i < 1000;i++)
+  rndarray[i]=~~(Math.random()*1000);
+
+console.log(rndarray)
+
+var sorted = skyline(rndarray);
+
+console.log(sorted)
